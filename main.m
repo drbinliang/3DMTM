2bf36ac03b31e24ca2d9fa31f9b1e13d52833082
1987hasit <bin.liang.ty@gmail.com>
2013-11-20 13:36:44 +1100

    update

--- main.m
+++ main.m
@@ -41,14 +41,14 @@ for i=1:length(files)
     % gesture representation using 3D-MTM
     representation = compute3DMTM(SEQUENCE);
    
-    % feature extraction using HOG (fast one)
-    features = feature_extraction_v3(representation);
+    % feature extraction using HOG (fast HOG)
+    features = feature_extraction(representation);
     
-    %% additional information
+    % additional information
     name = files{i};
     id = name(2:3);
 
-    %% save data
+    % save data
     TR_Gestures(i).Features = features;
     TR_Gestures(i).Name = name;
     TR_Gestures(i).Id = str2double(id);
@@ -56,107 +56,52 @@ for i=1:length(files)
     fprintf('done.\n');        
 end
 
-% %% PCA
-% NORM_TR_FEATURES = zscore(TR_FEATURES);
-% [COEFF, Proj_TR_Features, latent] = pca(NORM_TR_FEATURES);
-% 
-% for i=1:length(latent)
-%     if latent(i) / sum(latent) > 0.99
-%         break;
-%     end
-% end
-% 
-% pc_num = i;
-% 
-% REDUCED_TR_Features = Proj_TR_Features(:, 1:pc_num);
-
 % save training data as .mat file
-save('TR_Gestures.mat', 'TR_Gestures');
+% save('TR_Gestures.mat', 'TR_Gestures');
 
 % format to SVM file
 TR_SVM_file = 'TR_Gestures.svm';
-% mat2SVMfile(TR_Gestures, TR_SVM_file);
-
-% format normalized features
-X = [tr_lables zscore(TR_FEATURES)];
-mat2SVMfile_norm(X, TR_SVM_file);
+mat2SVMfile(TR_Gestures, TR_SVM_file);
 
 %pause;
 
-%% load test data
+%% Load test data
 d = dir(test_data_dir);
 isfile = [d(:).isdir] ~= 1;
 files = {d(isfile).name}';
 
 TE_Gestures = struct;
 
-%%
-TE_FEATURES = zeros(length(files), num_features);
-te_lables = zeros(length(files), 1);
-%%
+%% Feature extraction for test dataset
 fprintf('Loading test data:\n');
 for i=1:length(files)
     fprintf([files{i}, '...']);
     
+    % load data as a video sequence
     SEQUENCE = readDepthDataset([test_data_dir files{i}]);    
-    [SEQUENCE_XOZ, SEQUENCE_YOZ] = projectVideo(SEQUENCE);
-    
-    %% video filtered by LoG
-    FilteredVideo_XOY = LoGFilterVideo(SEQUENCE);
-    FilteredVideo_XOZ = LoGFilterVideo(SEQUENCE_XOZ);
-    FilteredVideo_YOZ = LoGFilterVideo(SEQUENCE_YOZ);
-    
-    %% generate representation for each gesture
-    % load data and extract features
-    
-    % video filtered by Gaussian filter [1 4 6 4 1]
-%     FilteredVideo = GaussianFilterVideo(SEQUENCE);
-%     FilteredVideo_XOZ = GaussianFilterVideo(SEQUENCE_XOZ);
-%     FilteredVideo_YOZ = GaussianFilterVideo(SEQUENCE_YOZ);
 
-    %% gesture representation
-    %3D-MTM
-%     representation = compute3DMTM_v2(SEQUENCE);   
-%     features = feature_extraction_v2(representation);
-%     representation = compute3DMTM_v3(FilteredVideo, FilteredVideo_XOZ, FilteredVideo_YOZ);
-   
-    representation = compute3DMTM_v4(FilteredVideo_XOY, FilteredVideo_XOZ, FilteredVideo_YOZ);
-    features = feature_extraction_v3(representation);
+    % gesture representation using 3D-MTM
+    representation = compute3DMTM(SEQUENCE);   
+    features = feature_extraction(representation);
 
-%     % MHI
-%     representation = computeMHI(SEQUENCE);
-%     features = HOG(representation);
-    %% additional information
+    % additional information
     name = files{i};
     id = name(2:3);
 
-    %% save data
+    % save data
     TE_Gestures(i).Features = features;
     TE_Gestures(i).Name = name;
     TE_Gestures(i).Id = str2double(id);
 
     fprintf('done.\n');
-
-    %% save to file
-    te_lables(i, :) = str2double(id);
-    TE_FEATURES(i, :) = features;
 end
 
-% %% PCA
-% NORM_TR_FEATURES = zscore(TE_FEATURES);
-% Proj_TE_Features = bsxfun(@minus, NORM_TR_FEATURES, mean(NORM_TR_FEATURES)) * COEFF;
-% REDUCED_TE_Features = Proj_TE_Features(:, 1:pc_num);
-
 % save test data as .mat file
-save('TE_Gestures.mat', 'TE_Gestures');
+% save('TE_Gestures.mat', 'TE_Gestures');
 
 % format to SVM file
 TE_SVM_file = 'TE_Gestures.svm';
-%mat2SVMfile(TE_Gestures, TE_SVM_file);
-
-% format normalized features
-X = [te_lables zscore(TE_FEATURES)];
-mat2SVMfile_norm(X, TE_SVM_file);
+mat2SVMfile(TE_Gestures, TE_SVM_file);
 
 %pause;
 
